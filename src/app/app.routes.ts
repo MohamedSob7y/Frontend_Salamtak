ac94a9191fce1180a06d03574c61a088dda29e08
Philopateer <fentesar9@gmail.com>
2026-04-23 20:18:17 +0200

    Add payment and payment confirmation pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login';
 import { RegisterComponent } from './features/auth/register/register';
-
 import { ForgotPasswordComponent } from './features/auth/forgot-password/forgot-password';
-
 import { AboutComponent } from './features/about/about/about';
-
 import { HomeComponent } from './features/home/home/home';
-
 import { PatientDashboardComponent } from './features/patient/dashboard/dashboard';
-
 import { DoctorDashboardComponent } from './features/doctor/dashboard/dashboard';
 
 export const routes: Routes = [
@@ -18,9 +13,7 @@ export const routes: Routes = [
 
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-
   { path: 'forgot-password', component: ForgotPasswordComponent },
-
 
   {
     path: 'patient/appointments',
@@ -50,20 +43,19 @@ export const routes: Routes = [
         .then(m => m.ProfileComponent)
   },
 
-
- 
   {
     path: 'patient/doctor/:id',
     loadComponent: () =>
       import('./features/patient/doctor-profile/doctor-profile')
         .then(m => m.DoctorProfileComponent)
   },
-{
-  path: 'chatbot',
-  loadComponent: () =>
-    import('./features/chatbot/chatbot')
-      .then(m => m.ChatbotComponent)
-},
+
+  {
+    path: 'chatbot',
+    loadComponent: () =>
+      import('./features/chatbot/chatbot')
+        .then(m => m.ChatbotComponent)
+  },
 
   { path: 'about', component: AboutComponent },
 
@@ -78,13 +70,21 @@ export const routes: Routes = [
   },
 
   {
+    path: 'patient/payment',
+    loadComponent: () =>
+      import('./features/patient/payment/payment')
+        .then(m => m.PaymentComponent)
+  },
+
+  {
+    path: 'patient/payment-confirmation',
+    loadComponent: () =>
+      import('./features/patient/payment-confirmation/payment-confirmation')
+        .then(m => m.PaymentConfirmationComponent)
+  },
+
+  {
     path: '**',
     redirectTo: ''
   }
-
 ];
-
-
-
-
-
