bb1f87f22f53a899035f2f1cad5422558b42121e
Mohamed Sobhy <moamedsobhy7@gmail.com>
2026-04-28 21:46:05 +0300

    Change

--- src/app/features/patient/payment-confirmation/payment-confirmation.ts
+++ src/app/features/patient/payment-confirmation/payment-confirmation.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-payment-confirmation',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule],
   templateUrl: './payment-confirmation.html',
-  styleUrl: './payment-confirmation.scss'
+  styleUrls: ['./payment-confirmation.scss']
 })
-export class PaymentConfirmationComponent {}
+export class PaymentConfirmationComponent {
+  payment: any;
+today = new Date().toLocaleDateString('en-GB', {
+  day: 'numeric',
+  month: 'short',
+  year: 'numeric'
+}) + ' • ' + new Date().toLocaleTimeString([], {
+  hour: '2-digit',
+  minute: '2-digit'
+});
+  constructor(private router: Router) {
+    this.payment = JSON.parse(localStorage.getItem('payment') || '{}');
+  }
+  goDashboard() {
+    this.router.navigate(['/patient/dashboard']);
+  }
+  downloadReceipt() {
+  const data = document.querySelector('.card') as HTMLElement;
+
+  if (!data) {
+    return;
+  }
+
+  html2canvas(data).then(canvas => {
+    const imgWidth = 190;
+    const imgHeight = (canvas.height * imgWidth) / canvas.width;
+    const imgData = canvas.toDataURL('image/png');
+
+    const pdf = new jsPDF('p', 'mm', 'a4');
+
+    pdf.addImage(imgData, 'PNG', 10, 10, imgWidth, imgHeight);
+    pdf.save('receipt.pdf');
+  });
+}
+}
--- src/app/features/patient/payment/payment.ts
+++ src/app/features/patient/payment/payment.ts
@@ -1,12 +1,70 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-payment',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, FormsModule,RouterModule],
   templateUrl: './payment.html',
-  styleUrl: './payment.scss'
+  styleUrls: ['./payment.scss']
 })
-export class PaymentComponent {}
+export class PaymentComponent {
+
+  selectedMethod: string = 'card';
+  showModal = false;
+
+  cardName = '';
+  cardNumber = '';
+  expiry = '';
+  cvv = '';
+
+  consultation = 450;
+  service = 75;
+  tax = 73.5;
+
+  get total() {
+    return this.consultation + this.service + this.tax;
+  }
+
+  constructor(private router: Router) {}
+
+  selectMethod(method: string) {
+    this.selectedMethod = method;
+  }
+
+  openConfirm() {
+    if (this.selectedMethod === 'card') {
+      if (!this.cardName || !this.cardNumber || !this.expiry || !this.cvv) {
+        alert('Please fill all card data');
+        return;
+      }
+    }
+    this.showModal = true;
+  }
+
+  confirmPayment() {
+    localStorage.setItem('payment', JSON.stringify({
+      total: this.total,
+      id: 'TXN' + Math.floor(Math.random() * 1000000)
+    }));
+
+    this.router.navigate(['/patient/payment-confirmation']);
+  }
+
+  cancelPayment() {
+    this.showModal = false;
+  }
+goToAppointment() {
+  this.router.navigate(['/patient/book-appointment']);
+}
+  
+  payVodafone() {
+    alert('Redirect to Vodafone Cash');
+  }
+
+  payFawry() {
+    alert('Redirect to Fawry');
+  }
+}
